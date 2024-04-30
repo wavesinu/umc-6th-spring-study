14e0aaeffce56854be330cc7a1453e9e2e51eb24
ISU <ourwavelets@gmail.com>
2024-04-30 17:51:48 +0900

    Update BaseCode.java
    - 잘못된 메서드명 수정

--- src/main/java/umc/study/apiPayload/code/BaseCode.java
+++ src/main/java/umc/study/apiPayload/code/BaseCode.java
@@ -4,5 +4,5 @@ public interface BaseCode {
     //status에서 두 개의 메서드를 오버라이드 할 것을 강제
     public ReasonDTO getReason();
 
-    public ReasonDTO getReasonHttpMethod();
+    public ReasonDTO getReasonHttpStatus();
 }
